7e82987a0f674f2f1c0ce852e22282e8dcc8a876
홍성준 <freevue@drx.gg>
2022-02-06 20:11:41 +0900

    [bugfix] 버그 수정

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 const 지원하는_타입 = ['숫자!', '글자!']
 const 변수_모음 = new Map()
+const 출력_꾸밈 = '요!'
 
-const 콘솔 = (...값: (string | number)[]) => console.log('야옹', ...값)
+const 콘솔 = (값: (string | number)) => console.log(출력_꾸밈, 값)
+const 출력 = (값: (string | number)) => {
+  콘솔(값)
+
+  return `${출력_꾸밈} ${값}`
+}
 const 값이_없는_경우_메세지 = (값: string | number) => `"${값}"? 뭐지 버근가?`
 const 수식_계산 = async (코드: string) => {
   if (/^.+(?:[+-/*].+)+$/g.test(코드)) {
@@ -26,7 +32,7 @@ const 변수_모음_체크 = (변수명: string) => {
   return 값
 }
 const 조각_유효성_검사 = (코드: string, 정규식: RegExp) => {
-  const 조각들 = 정규식.exec(코드)
+  const 조각들 = 정규식.exec(코드.trim())
 
   if (조각들 === null)
     throw new Error('죠졌네 잉거')
@@ -36,8 +42,8 @@ const 조각_유효성_검사 = (코드: string, 정규식: RegExp) => {
   return 조각들.map((조각) => 조각.trim())
 }
 const 라인_실행 = async (코드: string) => {
-  if (/^\s+허어/g.test(코드)) {
-    const [선언부, 타입] = 조각_유효성_검사(코드, /^\s+허어(.+)(.{2}!)$/g)
+  if (/^허어/g.test(코드)) {
+    const [선언부, 타입] = 조각_유효성_검사(코드, /^허어(.+)(.{2}!)$/g)
 
     if (!지원하는_타입.includes(타입))
       throw new Error(값이_없는_경우_메세지(타입))
@@ -58,21 +64,23 @@ const 라인_실행 = async (코드: string) => {
     }
   }
 
-  if (/^\s+야옹/g.test(코드)) {
-    const [선언부] = 조각_유효성_검사(코드, /^\s+야옹(.+)$/g)
+  if (/^야옹/g.test(코드)) {
+    const [선언부] = 조각_유효성_검사(코드, /^야옹(.+)$/g)
     const 일반_선언부 = await 수식_계산(선언부)
 
-    if (!isNaN(Number(일반_선언부)))
-      return 콘솔(Number(일반_선언부))
+    if (!isNaN(Number(일반_선언부))) {
+      return 출력(Number(일반_선언부))
+    }
 
-    return 콘솔(변수_모음_체크(일반_선언부)['값'])
+    return 출력(변수_모음_체크(일반_선언부)['값'])
   }
-
-  return ''
 }
 
-const 가자 = async (코드: string) => {
-  const 라인들 = 코드.trim().split('\n')
+export const 가자 = async (코드: string) => {
+  const 라인들 = 코드
+    .trim()
+    .split('\n')
+    .map((라인) => 라인.trim())
 
   if (라인들.shift() !== '가자!' || 라인들.pop() !== '알게모에요')
     throw new Error('죠졌네 잉거')
@@ -91,4 +99,4 @@ const 파일_유효성_검사 = async (파일: string) => {
   return await Deno.readTextFile(파일)
 }
 
-await 가자(await 파일_유효성_검사(Deno.args[0]))
+if (Deno.args[0]) await 가자(await 파일_유효성_검사(Deno.args[0]))
