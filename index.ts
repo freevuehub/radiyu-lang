c98929ab65a7ad7604e0fe9615bae6dc27972fd3
freevuehub <hackish113@gmail.com>
2022-02-13 20:34:14 +0900

    [refactor] 소스 분리

--- index.ts
+++ index.ts
@@ -1,43 +1,6 @@
-type 지원하는_타입 = '숫자!' | '글자!' | '됐다!' | '다이노소어!'
+import 라디유_컨텍스트 from './module/context.ts'
 
-class 라디유_컨텍스트 {
-  허어어: {
-    [key: string]: {
-      값: any
-      타입: 지원하는_타입
-    }
-  } = {}
-
-  set 허어({ 이름, 값, 타입 }: { 이름: string, 값: any, 타입: 지원하는_타입 }) {
-    const 변환된_값 = (): string | number | boolean | undefined => {
-      switch (타입) {
-        case '다이노소어!':
-          return `${값}`
-        case '글자!':
-          return `${값}`
-        case '됐다!':
-          return 값 === '됐어!'
-            ? true
-            : 값 === 'ㅈ됐어!'
-              ? false
-              : undefined
-        case '숫자!':
-          return isNaN(Number(값)) ? undefined : Number(값)
-        default:
-          return undefined
-      }
-    }
-
-    if (변환된_값() === undefined)
-      throw new Error('라창났다!!')
-
-    this.허어어[이름] = { 값: 변환된_값(), 타입 }
-  }
-
-  get 야옹() {
-    return (이름: string): { 값: any, 타입: 지원하는_타입 } => this.허어어[이름]
-  }
-}
+import type { 지원하는_타입 } from './type.ts'
 
 let 입력중인_킹짱룡 = ''
 let 조건문_실행중 = false
